91919473fd288848479884a962ac38249e015b93
Adnan Mohamed <adnan.hashem.bh@gmail.com>
2023-11-17 10:33:50 +0300

    Add comments

--- src/public/client.js
+++ src/public/client.js
@@ -1,10 +1,3 @@
-// let store = Immutable.Map({
-//   rovers: ["Curiosity", "Opportunity", "Spirit"], // available rover names
-//   selectedRover: "", // which of the existing rovers is selected by the user
-//   rover: {}, // the information of the selected rover
-//   photos: [], // an array of photos containing the source of the photo and other info about each photo.
-// });
-
 // State Initialization
 // Initialize the application state with Immutable.Map to maintain an immutable application state.
 const store = Immutable.Map({
@@ -27,10 +20,40 @@ const store = Immutable.Map({
 
 const root = document.getElementById("root");
 
+/**
+ * Updates the application's state and re-renders the UI.
+ *
+ * This function is responsible for updating the state of the application with new data and
+ * ensuring that the UI reflects these changes. It merges the new state with the existing one,
+ * keeping the values from the new state and retaining any values from the old state that are not
+ * overridden.
+ *
+ * @param {Immutable.Map | Object} store - The current state of the application. This can be an
+ *                                          Immutable.Map or a regular JavaScript object with the
+ *                                          same structure as the initial state.
+ * @param {Immutable.Map | Object} newState - An Immutable.Map or JavaScript object containing
+ *                                            the updated state properties. These properties will
+ *                                            override the corresponding ones in the current state.
+ *
+ * Return Value:
+ * - No return value.
+ */
+
 const updateStore = (store, newState) => {
   store = store.merge(newState);
   render(root, store.toJS());
 };
+
+/**
+ * Updates the selected rover in the state and fetches its data.
+ *
+ * This function is triggered when a user selects a rover from the UI. It fetches data for the
+ * selected rover and updates the application state with this new data, including the rover's
+ * name, its information, and recent photos.
+ *
+ * @param {string} roverName - The name of the rover selected by the user.
+ *
+ */
 
 const updateSelectedRover = (roverName) => {
   fetchRoverData(roverName)
@@ -42,9 +65,30 @@ const updateSelectedRover = (roverName) => {
     });
 };
 
+/**
+ * Renders the application UI in the specified root element.
+ *
+ * This function is responsible for rendering the entire application UI based on the current
+ * state. It dynamically generates HTML content and inserts it into the root DOM element.
+ *
+ * @param {HTMLElement} root - The root DOM element where the application UI is rendered.
+ * @param {Object} state - The current state of the application.
+ *
+ */
 const render = (root, state) => {
   root.innerHTML = App(state);
 };
+
+/**
+ * Fetches data for a specified rover from the server.
+ *
+ * This function makes an API call to retrieve data for the given rover. The data includes
+ * rover details and a set of recent photos taken by the rover.
+ *
+ * @param {string} selectedRover - The name of the rover for which data is to be fetched.
+ * @returns {Promise<Object>} A promise that resolves to the rover data.
+ *
+ */
 
 const fetchRoverData = (selectedRover) => {
   return getRoverData(selectedRover)
@@ -69,9 +113,22 @@ const fetchRoverData = (selectedRover) => {
     .catch(console.error);
 };
 
+/**
+ * Initializes the application when the window loads.
+ * This function sets up the initial state of the application and triggers the first render
+ * of the UI components.
+ */
 window.onload = () => {
   render(root, store.toJS());
 };
+
+/**
+ * Creates HTML content for displaying information about the selected rover.
+ *
+ * @param {Object} rover - Object containing details of the selected rover including
+ *                         launchDate, landingDate, and status.
+ * @returns {string} HTML string representing the rover's information in a card layout.
+ */
 
 const RoverInfo = ({ landingDate, launchDate, status }) => {
   if (landingDate && launchDate && status) {
@@ -92,7 +149,13 @@ const RoverInfo = ({ landingDate, launchDate, status }) => {
   }
 };
 
-// create content
+/**
+ * Main function to construct and render the application's UI.
+ *
+ * @param {Object} state - The current state of the application, including rover data and
+ *                         photos.
+ * @returns {string} HTML string representing the entire application UI.
+ */
 const App = (state) => {
   let { rovers, selectedRover, rover, photos } = state;
   return `
@@ -105,10 +168,22 @@ const App = (state) => {
 
 // ------------------------------------------------------  COMPONENTS
 
+/**
+ * Generates HTML content for the page title.
+ *
+ * @param {string} title - The title text to be displayed.
+ * @returns {string} HTML string representing the page title as an uppercase header element.
+ */
 const PageTitle = (title) => {
   return `<h1 class="text-center py-3">${title.toUpperCase()}</h1>`;
 };
 
+/**
+ * Creates a gallery component for displaying rover photos.
+ *
+ * @param {Array<Object>} photos - An array of photo objects for the selected rover.
+ * @returns {string} HTML string representing a photo gallery layout.
+ */
 const RoverPhotoGallery = (photos) => {
   return `
         <section class="container">
@@ -120,6 +195,15 @@ const RoverPhotoGallery = (photos) => {
         </section>
     `;
 };
+
+/**
+ * Renders a single photo card.
+ *
+ * @param {Object} photo - The photo object containing the image source, camera name, and
+ *                         date taken.
+ * @param {string} title - The title for the photo card.
+ * @returns {string} HTML string representing a single photo card.
+ */
 
 const RenderCard = (photo, cardTitle) => {
   const src = photo.src;
@@ -135,6 +219,12 @@ const RenderCard = (photo, cardTitle) => {
         </div>`;
 };
 
+/**
+ * Generates a collection of photo cards for the gallery.
+ *
+ * @param {Array<Object>} photos - An array of photo objects to be rendered as cards.
+ * @returns {string} HTML string representing a collection of photo cards.
+ */
 const RenderCards = (roverPhotos) => {
   let result = roverPhotos
     .map((photo, i) => RenderCard(photo, `Photo #${i + 1}`))
@@ -142,11 +232,23 @@ const RenderCards = (roverPhotos) => {
   return result;
 };
 
+/**
+ * Handles the selection change event for the rover dropdown.
+ *
+ * @param {Event} event - The event object triggered on selection change.
+ */
 const selectOnChange = (e) => {
   const roverName = e.target.value;
   updateSelectedRover(roverName);
 };
 
+/**
+ * Constructs a dropdown selector for choosing a Mars rover.
+ *
+ * @param {Array<string>} rovers - An array of available rover names.
+ * @param {string} selectedRover - The currently selected rover name.
+ * @returns {string} HTML string representing a dropdown selector for rovers.
+ */
 const RoversSelector = (rovers, selected) => {
   return `
         <div id="selectorContainer" class="row mt-5 mb-3">
@@ -169,6 +271,12 @@ const RoversSelector = (rovers, selected) => {
 
 // ------------------------------------------------------  API CALLS
 
+/**
+ * Fetches detailed information and photos for a specified rover.
+ *
+ * @param {string} roverName - The name of the rover for which data is to be fetched.
+ * @returns {Promise<Object>} A promise resolving to the rover data.
+ */
 const getRoverData = (rover) => {
   return fetch(`http://localhost:3000/rovers/${rover}`)
     .then((res) => res.json())
